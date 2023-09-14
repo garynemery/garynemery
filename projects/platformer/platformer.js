47e1983c57cee23ab506ce513bba51b9968f8bc2
garyn emery <garyn.emery@sciacademy.org>
2023-09-14 15:11:43 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -48,15 +48,17 @@ $(function () {
     createPlatform(744,575,22,25)
     createPlatform(830,575,22,25)
     createPlatform(900,500,225,25)
-    createPlatform(730,360,225,25)
+    createPlatform(800,380,225,25)
+    createPlatform(1155,380,25,25)
+    createPlatform(1225,320,25,25)
+    createPlatform(1300,290,100,25)
 
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("star", 500, 300);
 
 
     // TODO 3
@@ -64,6 +66,7 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("top", 770, 1000); // cannon on top wall, 600px down, shooting once per second
 
 
 
